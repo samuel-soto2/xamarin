69f9c34b4de812338229ab5264b8e8914f2f0867
samuel-soto2 <1461888@senati.pe>
2023-09-21 17:02:56 -0500

    asignamos las distintas operaciones como suma, resta, multiplicación y división

--- apkCalculadora/apkCalculadora/MainPage.xaml.cs
+++ apkCalculadora/apkCalculadora/MainPage.xaml.cs
@@ -5,3 +5,19 @@ using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
 
+namespace apkCalculadora
+{
+    public partial class MainPage : ContentPage
+    {
+        double res = 0;
+        public MainPage()
+        {
+            InitializeComponent();
+            wkOperacion.Items.Add("suma");
+            wkOperacion.Items.Add("resta");
+            wkOperacion.Items.Add("multiplicacion");
+            wkOperacion.Items.Add("division");
+            wkOperacion.SelectedIndexChanged += WkOperacion_SelectedIndexChanged;
+            calcular.Clicked += Calcular_Clicked;
+
+        }
